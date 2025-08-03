60470bf189163aa899d64c440102a028edca4164
Sanika-Khadake <ramchandrakhadake1975@gmail.com>
2025-08-03 09:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (63.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,24 @@
  class Solution {
 
-  //Using recursion 
+  //Using dynamic programming
+   public int fibs(int n,  int[] dp){
+      if(n<=1){
+        return n;
+      }
+      if(dp[n]!=-1){
+        return dp[n];
+      }
+
+      dp[n]=fibs(n-1,dp)+fibs(n-2,dp);
+      return dp[n];
+   }
+
     public int fib(int n) {
-       if(n<=1){
-            return n;
+       int[] dp = new int[n+1];
+       for(int i=0 ;i <n+1 ;i++){
+            dp[i] = -1 ;
         }
-        return fib(n-1)+fib(n-2);
-    
+        return fibs(n,dp);   
     }
+    
 }
